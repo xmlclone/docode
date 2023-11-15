54b9d213f0ec2e039546a9c7f572562441fd9522
linlei <linlei@migu.cn>
2023-11-15 18:52:22 +0800

    add email demo

--- python_demo/xnetwork/email_demo0.py
+++ python_demo/xnetwork/email_demo0.py
@@ -1,30 +1,58 @@
+import smtplib
+import os
+
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import smtplib
 
-def send_mail(mail_host, mail_sender, mail_passwd, to_emailli, content):
-    '''
-        群发邮件功能
-    :param mail_host: 发件人SMTP服务器地址
-    :param mail_sender: 发件人账号
-    :param mail_passwd: 发件人密码
-    :param to_emailli: 收件人列表
-    :param content: 邮件内容
-    :return:
-    '''
-    msg = MIMEMultipart('related')
-    msg["Subject"] = "Amber卡顿监控告警（{}--{}）" .format(start_time, end_time) # 邮件主题
-    msg["From"] = mail_sender  # 发送人
-    msg["To"] = ';'.join(to_emailli)  # 接收人
 
-    msg.attach(MIMEText(content, 'html', 'utf-8'))
+def send_html_mail(
+    host: str, 
+    sender: str, 
+    sender_passwd: str, 
+    to_list: list[str],
+    subject: str,
+    content: str,
+    cc_list: list[str] = None,
+    port: int=25
+):
+    """群发邮件功能
 
-    ## 发送邮件
-    try:
-        s = smtplib.SMTP()  # 实例化对象
-        s.connect(mail_host, 25)  # 连接邮箱服务器，端口号为25
-        s.login(mail_sender, mail_passwd)  # 登录邮箱
-        s.sendmail(mail_sender, to_emailli, msg.as_string())
-        s.quit()
-    except Exception as e:
-        print(e)
+    :param host: 发件人SMTP服务器地址.
+    :type host: str
+    :param sender: 发件人账号.
+    :type sender: str
+    :param sender_passwd: 发件人密码.
+    :type sender_passwd: str
+    :param to_list: 收件人列表.
+    :type to_list: list[str]
+    :param subject: 邮件主题.
+    :type subject: str
+    :param content: 邮件内容, html格式.
+    :type content: str
+    :param cc_list: 抄送人列表.
+    :type cc_list: list[str]
+    :param port: 邮箱服务器Port.
+    :type port: int
+    """
+
+    message = MIMEMultipart('related')
+    message["Subject"] = subject
+    message["From"] = sender
+    message["To"] = ';'.join(to_list)
+    if cc_list:
+        message["Cc"] = ';'.join(cc_list)
+    message.attach(MIMEText(content, 'html', 'utf-8'))
+    with smtplib.SMTP(host, port) as smtp:
+        smtp.login(sender, sender_passwd)
+        smtp.send_message(message)
+
+
+passwd = os.environ.get("MYPASS")
+send_html_mail(
+    "mail.test.cn",
+    "linlei@test.cn",
+    passwd,
+    ["linlei@test.cn"],
+    "Test",
+    "<h1>Test content</h1>"
+)
